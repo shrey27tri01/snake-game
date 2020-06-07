e5dd802617603c760eea82cbafecf463332a79e3
shrey27tri01 <shreytripathi27@gmail.com>
2020-06-08 00:44:35 +0530

    added functionalities

--- p5.js
+++ p5.js
@@ -12,6 +12,7 @@ function setup () {
 
 function draw() {
     background(51);
+    snake.death();
     snake.update();
     snake.show();
 
@@ -53,8 +54,18 @@ class Snake {
         this.y = 0;
         this.xSpeed = 1;
         this.ySpeed = 0;
+        this.total = 0;
+        this.tail = [];
 
         this.update = function () {
+            if (this.total === this.tail.length) {
+                for (var i = 0; i < this.tail.length - 1; i++) {
+                    this.tail[i] = this.tail[i + 1];
+                }
+            }
+            
+            this.tail[this.total - 1] = createVector(this.x, this.y);
+
             this.x = this.x + this.xSpeed * scl;
             this.y = this.y + this.ySpeed * scl;
 
@@ -62,9 +73,21 @@ class Snake {
             this.y = constrain (this.y, 0, width - scl);
         };
 
+        this.death = function() {
+            for (var i = 0; i < this.tail.length; i++) {
+                var pos = this.tail[i];
+                var d = dist(this.x, this.y, pos.x, pos.y);
+                if (d < 1) {
+                    this.total = 0;
+                    this.tail = [];
+                }
+            }
+        }
+
         this.eat = function(pos) {
             var distance = dist(this.x, this.y, pos.x, pos.y);
             if (distance < 1) {
+                this.total++;
                 return true;
             }
             else {
@@ -74,6 +97,10 @@ class Snake {
 
         this.show = function () {
             fill(255);
+            for (var i = 0; i < this.tail.length; i++) {
+                rect(this.tail[i].x, this.tail[i].y, scl, scl);
+            }
+
             rect(this.x, this.y, scl, scl);
         };
 
